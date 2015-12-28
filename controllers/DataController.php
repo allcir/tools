ef4bda94a1745f9b8288dc9327e256ef597514dd
allcir <allcir@foxmail.com>
2015-12-28 16:55:11 +0800

    药导入

--- controllers/DataController.php
+++ controllers/DataController.php
@@ -7,14 +7,127 @@ use app\models\Buy6Old;
 use app\models\Buy6;
 use app\models\BuyData6;
 use app\models\Area;
+use app\models\Info22Old;
+use app\models\Info22;
+use app\models\InfoData22;
 
 class DataController extends Controller{
 
-	public function actionAgency(){
+	public function actionDrug(){
 		
+		$old = new Info22Old();
+		
+		$offset = isset($_GET['offset']) ? $_GET['offset'] : '25541';
+		
+		$rs = $old->find()->offset($offset)->limit(50)->orderBy('ID ASC')->all();
+
+		if($rs){
+				
+			foreach($rs as $v){
+		
+				$info = new Info22();
+				$infodata = new InfoData22();
+
+				$info->itemid = $v->ID;
+				$info->level = 0;
+				$info->title = $v->Name;
+				$info->hits = $v->HitCount;
+
+				$info->username = $v->UserName;
+
+
+				$info->addtime = strtotime($v->PostDTime);
+				$info->edittime = $info->addtime;
+				$info->editdate = date('Y-m-d',$info->addtime);
+				$info->adddate = date('Y-m-d',$info->addtime);
+				
+
+				$info->company = $v->Company;
+				$info->vip = $v->VIP;
+
+
+
+				$info->status = 3;
+				$info->listorder = 0;
+				$info->islink = 0;
+
+				$info->approvalno = $v->ApprovalNO;
+				$info->zs_uid = $v->Uid;
+				$info->zs_vip = $v->VIP;
+				$info->zs_bid = $v->Bid;
+				$info->zs_bidarea = $v->BidArea;
+				$info->zs_area = $v->Area;
+				$info->zs_imgurl = $v->ImgUrl;
+				$info->zs_imgurl2 = $v->ImgUrl2;
+				$info->zs_imgurl3 = $v->ImgUrl3;
+				$info->zs_imgurl4 = $v->ImgUrl4;
+				$info->zs_medicare = $v->Medicare;
+				$info->zs_tags = $v->Tags;
+				$info->zs_class_lv1 = $v->Class_LV1;
+				$info->zs_class_lv2 = $v->Class_LV2;
+				$info->zs_class_lv3 = $v->Class_LV3;
+				$info->zs_customclass = $v->CustomClass;
+				$info->zs_advantage = $v->Advantage;
+				$info->zs_dtime = $v->DTime;
+				$info->zs_postdtime = $v->PostDTime;
+				$info->zs_hitcount = $v->HitCount;
+				$info->zs_state = $v->State;
+				$info->zs_sitename = $v->SiteName;
+				$info->zs_replycount = $v->ReplyCount;
+				$info->zs_html = $v->Html;
+				$info->zs_letter = $v->Letter;
+				$info->zs_isimg = $v->IsImg;
+				$info->zs_isregular = $v->IsRegular;
+				$info->zs_orderno = $v->OrderNO;
+				$info->zs_upkey = $v->UpKey;
+				$info->zs_uporderno = $v->UpOrderNO;
+				$info->zs_license = $v->License;
+				$info->zs_norms = $v->Norms;
+				$info->zs_exhibition = $v->Exhibition;
+
+				$info->point = str_replace(',', ' ', $v->Advantage);
+
+				$info->zszc = is_null($v->Support) ? '': $v->Support;
+				$info->dlsyq = $v->Require;
+				$info->memo = $v->Memo;
+				$info->dosageform = is_null($v->DosageForm) ? '': $v->DosageForm;
+				$info->effect = $v->Effect;
+				$info->explain = $v->Explain;
+				$info->standard = $v->Standard;
+				$info->direction = $v->Direction;
+				$info->dosage = $v->Dosage;
+				$info->type = $v->Type;
+				$info->refreshnum = $v->RefreshNum;
+				$info->refreshtime = $v->DTime;
+				$info->is_zs = 1;				
+		
+				$infodata->itemid = $v->ID;
+		
+				if($v->Intro == null){
+					$v->Intro = '';
+				}
+				$infodata->content = $v->Intro;
+		
+				$info->save(false);
+				$infodata->save(false);
+		
+			}
+				
+			$num = $offset + 50;
+		
+			return $this->render('drug',['num'=>$num]);
+				
+		}else{
+			echo '0k!!!!!!!!!!';
+		}
+		
+	}
+	
+	public function actionAgency(){
+		exit();
 		$old = new Buy6Old();
 		
-		$offset = isset($_GET['offset']) ? $_GET['offset'] : '355249';
+		$offset = isset($_GET['offset']) ? $_GET['offset'] : '384036';
 		
 		$rs = $old->find()->offset($offset)->limit(500)->orderBy('ID ASC')->all();
 		
